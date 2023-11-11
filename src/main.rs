224c08dac5e18679d5fe1f8f2b37f9ee2dd75062
Duncan Sung W. Kim <DuncanSungWKim@gmail.com>
2023-11-11 21:02:41 +0900

    Get a PDF file name from the first argument of the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{
+    env
+};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let fileName = GetPDFFileName() ;
+    println!("Hello, {}!", fileName );
+}
+
+
+
+fn GetPDFFileName()
+-> String
+{
+    let arg1 = env::args().nth( 1 ) ;
+    arg1.expect( "PDF file as first argument" )
 }
