d04516193451d5a6720c47a3820181476fcf3808
Duncan Sung W. Kim <DuncanSungWKim@gmail.com>
2023-11-12 00:25:37 +0900

    Create struct ImageCollector with a folder for saving images

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::{
     env
+,   path::Path, fs, io::prelude::*
 };
 
 
 
 fn main() {
     let fileName = GetPDFFileName() ;
-    println!("Hello, {}!", fileName );
+    let mut imageCollector = ImageCollector::newWithSavingFolder( fileName.as_str() ) ;
 }
 
 
@@ -16,4 +17,30 @@ fn GetPDFFileName()
 {
     let arg1 = env::args().nth( 1 ) ;
     arg1.expect( "PDF file as first argument" )
+}
+
+
+
+struct ImageCollector
+{
+    folderName : String
+}
+
+
+impl ImageCollector
+{
+    fn newWithSavingFolder(
+        fileName : &str
+    )-> Self
+    {
+        let folderName = Path::new( fileName ).file_stem().unwrap() ;
+        let folder = Path::new( folderName ) ;
+        if folder.is_dir() {
+            fs::remove_dir_all( folder ) ;
+        }
+        fs::create_dir( folder ) ;
+        Self {
+            folderName : folderName.to_str().unwrap().to_owned()
+        }
+    }
 }
